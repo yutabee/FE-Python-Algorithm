4afb87b8bd835f61e41ef20b5f6f49b8fdcd4522
yutabee <bsk87m@gmail.com>
2023-04-04 13:10:57 +0900

    🎨 [modify] refactoring

--- 04-insertion-sort/insertion-sort.py
+++ 04-insertion-sort/insertion-sort.py
@@ -1,21 +1,52 @@
+def print_colored_array(arr, sorted_end):
+    """
+    ソート済みの部分と未ソートの部分をカラー分けして出力する関数。
+
+    :param arr: 整列対象のリスト
+    :param sorted_end: ソート済みの部分の末尾のインデックス
+    """
+    green = "\033[32m"
+    red = "\033[31m"
+    reset = "\033[0m"
+
+    sorted_part = f"{green}{' '.join(map(str, arr[:sorted_end + 1]))}{reset}"
+    unsorted_part = f"{red}{' '.join(map(str, arr[sorted_end + 1:]))}{reset}"
+    print(f"{sorted_part} {unsorted_part}")
+
+
+def insert_key(arr, i):
+    """
+    未ソート部分の先頭要素（キー）をソート済み部分に適切な位置に挿入する関数。
+
+    :param arr: 整列対象のリスト
+    :param i: 未ソート部分の先頭要素のインデックス
+    """
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = key
+
+
 def insertion_sort(arr):
+    """
+    リストを挿入ソートで整列する関数。
+
+    :param arr: 整列対象のリスト
+    """
     n = len(arr)
 
     for i in range(1, n):
-        key = arr[i]  # 未ソート部分の先頭要素
-        j = i - 1
+        print(f"i: {i}, arr: {arr}")
+        print_colored_array(arr, i - 1)
 
-        print(f"i: {i}, key: {key}, j: {j}")  # デバッグ出力1
+        insert_key(arr, i)
 
-        # ソート済み部分を右から左に走査し、適切な位置に挿入
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-
-            print(f"  j: {j}, arr: {arr}")  # デバッグ出力2
-
-        arr[j + 1] = key  # 未ソート部分の先頭要素を適切な位置に挿入
-        print(f"arr after step {i}: {arr}\n")  # デバッグ出力3
+        print(f"arr after step {i}: {arr}\n")
+        print_colored_array(arr, i)
 
 # 使用例
 arr = [64, 34, 25, 12, 22, 11, 90]
