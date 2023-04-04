bf6d5cb2678d9b03d83c7e46430e7b6901bd319f
yutabee <bsk87m@gmail.com>
2023-04-04 23:23:59 +0900

    🚀 再帰関数を用いないユークリッドの互除法

--- 08-euclidean/euclidean_algorithm_1.py
+++ 08-euclidean/euclidean_algorithm_1.py
@@ -1,11 +1,25 @@
-print("a≧bとなる自然数を入力してください")
-a = int(input("a="))
-b = int(input("b="))
+def euclidean_algorithm(a: int, b: int) -> int:
+    """
+    2つの整数 a, b (a >= b)の最大公約数を求める。
 
-while True:
-    r = a%b
-    if r == 0:
-        print("それらの数の最大公約数は", b)
-        break
-    a = b
-    b = r
+    Args:
+        a: 整数
+        b: 整数
+
+    Returns:
+        a, b の最大公約数
+    """
+    while b != 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
+
+if __name__ == "__main__":
+    print("a≧bとなる自然数を入力してください")
+    a = int(input("a="))
+    b = int(input("b="))
+    gcd = euclidean_algorithm(a, b)
+    print("それらの数の最大公約数は", gcd)
+
