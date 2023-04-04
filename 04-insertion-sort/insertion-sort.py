4605701c6490448c8ae1f52f12e250753e5b9f97
yutabee <bsk87m@gmail.com>
2023-04-04 14:18:30 +0900

    🎨 [modify] 出力を変更

--- 04-insertion-sort/insertion-sort.py
+++ 04-insertion-sort/insertion-sort.py
@@ -1,54 +1,44 @@
-def print_colored_array(arr, sorted_end):
-    """
-    ソート済みの部分と未ソートの部分をカラー分けして出力する関数。
+def print_colored_array(arr, sorted_end, i=None, j=None):
+    def format_element(index, value):
+        element = f"{value}"
+        if index == j:
+            element = f"j:{element}"
+        if index == i:
+            element = f"i:{element}"
+        return element
 
-    :param arr: 整列対象のリスト
-    :param sorted_end: ソート済みの部分の末尾のインデックス
-    """
     green = "\033[32m"
     red = "\033[31m"
     reset = "\033[0m"
 
-    sorted_part = f"{green}{' '.join(map(str, arr[:sorted_end + 1]))}{reset}"
-    unsorted_part = f"{red}{' '.join(map(str, arr[sorted_end + 1:]))}{reset}"
-    print(f"{sorted_part} {unsorted_part}")
+    colored_elements = [format_element(index, value) for index, value in enumerate(arr)]
+    sorted_part = f"{green}{' '.join(colored_elements[:sorted_end + 1])}{reset}"
+    unsorted_part = f"{red}{' '.join(colored_elements[sorted_end + 1:])}{reset}"
+
+    return f"{sorted_part} {unsorted_part}"
 
 
 def insert_key(arr, i):
-    """
-    未ソート部分の先頭要素（キー）をソート済み部分に適切な位置に挿入する関数。
-
-    :param arr: 整列対象のリスト
-    :param i: 未ソート部分の先頭要素のインデックス
-    """
     key = arr[i]
     j = i - 1
 
     while j >= 0 and arr[j] > key:
         arr[j + 1] = arr[j]
         j -= 1
+        print(print_colored_array(arr, j, i, j))
 
     arr[j + 1] = key
 
 
 def insertion_sort(arr):
-    """
-    リストを挿入ソートで整列する関数。
-
-    :param arr: 整列対象のリスト
-    """
     n = len(arr)
 
     for i in range(1, n):
-        print(f"i: {i}, arr: {arr}")
-        print_colored_array(arr, i - 1)
-
+        print(f"Before step {i}: {print_colored_array(arr, i - 1, i)}")
         insert_key(arr, i)
+        print(f"After step {i}: {print_colored_array(arr, i)}\n")
 
-        print(f"arr after step {i}: {arr}\n")
-        print_colored_array(arr, i)
 
-# 使用例
 arr = [64, 34, 25, 12, 22, 11, 90]
 insertion_sort(arr)
-print("Sorted array is:", arr)  # Sorted array is: [11, 12, 22, 25, 34, 64, 90]と出力される
+print("Sorted array is:", arr)
