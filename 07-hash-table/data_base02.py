ea6163720b545570026d161f3bb1f27922fbabb3
yutabee <bsk87m@gmail.com>
2023-04-04 22:36:52 +0900

    🚀 Created Hash-Table-Class

--- 07-hash-table/data_base02.py
+++ 07-hash-table/data_base02.py
@@ -1,61 +1,68 @@
-HASH = 5
-name = [None]*HASH
-tel = [None]*HASH
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.names = [None] * size
+        self.phone_numbers = [None] * size
 
-def hash(key):
-    h = 0
-    for i in key:
-        h = h + ord(i)
-    return h%HASH
+    def calculate_hash(self, key):
+        return sum(ord(char) for char in key) % self.size
 
-def open_add(n, t, h):
-    flg = False
-    for i in range(HASH-1):
-        h = (h+1)%HASH
-        if name[h] == None:
-            name[h] = n
-            tel[h] = t
-            flg = True
-            print("再ハッシュ", h, "データ格納完了")
-            break
-    if flg == False:
-        print("データを格納できる領域がありません")
-
-def serch_rehash(key, h):
-    for i in range(HASH-1):
-        h = (h+1)%HASH
-        if name[h] == key:
-            return h
-    return -1
-
-while True:
-    n = input("名前を入力してください ")
-    if n == "":
-        break
-    t = input("番号を入力してください ")
-    h = hash(n)
-    if name[h] == None:
-        name[h] = n
-        tel[h] = t
-        print("ハッシュ値", h, "データ格納完了")
-    else:
-        open_add(n, t, h)
-
-print(name)
-print(tel)
-
-while True:
-    n = input("検索する名前は? ")
-    if n == "":
-        break
-    h = hash(n)
-    if name[h] == n:
-        print(n+"さんの番号は"+tel[h])
-    elif name[h] == "None":
-        print("その名前は登録されていません")
-    else:
-        h = serch_rehash(n, h)
-        if h == -1:
-            print("その名前は登録されていません")
+    def insert(self, name, phone_number):
+        hash_index = self.calculate_hash(name)
+        if self.names[hash_index] is None:
+            self.names[hash_index] = name
+            self.phone_numbers[hash_index] = phone_number
+            print(f"Hash value: {hash_index}, data stored.")
         else:
-            print(n+"さんの番号は"+tel[h])
+            self.open_addressing(name, phone_number, hash_index)
+
+    def open_addressing(self, name, phone_number, hash_index):
+        for _ in range(self.size - 1):
+            hash_index = (hash_index + 1) % self.size
+            if self.names[hash_index] is None:
+                self.names[hash_index] = name
+                self.phone_numbers[hash_index] = phone_number
+                print(f"Rehashed: {hash_index}, data stored.")
+                return
+        print("No available space to store data.")
+
+    def search(self, name):
+        hash_index = self.calculate_hash(name)
+        if self.names[hash_index] == name:
+            return self.phone_numbers[hash_index]
+        elif self.names[hash_index] is None:
+            return None
+        else:
+            return self.search_rehash(name, hash_index)
+
+    def search_rehash(self, name, hash_index):
+        for _ in range(self.size - 1):
+            hash_index = (hash_index + 1) % self.size
+            if self.names[hash_index] == name:
+                return self.phone_numbers[hash_index]
+        return None
+
+
+# Entry Program
+if __name__ == "__main__":
+    hash_table = HashTable(5)
+
+    while True:
+        name = input("Enter name: ")
+        if name == "":
+            break
+        phone_number = input("Enter phone number: ")
+        hash_table.insert(name, phone_number)
+
+    print(hash_table.names)
+    print(hash_table.phone_numbers)
+
+    while True:
+        name = input("Enter name to search: ")
+        if name == "":
+            break
+        phone_number = hash_table.search(name)
+        if phone_number is None:
+            print(f"{name} is not registered.")
+        else:
+            print(f"{name}'s phone number is {phone_number}.")
