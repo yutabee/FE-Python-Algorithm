cfa218d6778da2bc14d627822e51ce191dce30fb
yutabee <bsk87m@gmail.com>
2023-04-16 14:51:06 +0900

    🎨 [modify] insertion_sort2.py 交換条件を分かりやすくするため配列と同じ順列に修正

--- 04-insertion-sort/modules/insertion_sort2.py
+++ 04-insertion-sort/modules/insertion_sort2.py
@@ -6,7 +6,7 @@ def insertion_sort(arr: List[int]) -> List[int]:
 
     for i in range(1, n):
         for j in range(i, 0, -1):
-            if arr[j] < arr[j - 1]:
+            if arr[j - 1] < arr[j]:
                 tmp = arr[j]
                 arr[j] = arr[j - 1]
                 arr[j - 1] = tmp
